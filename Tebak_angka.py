a0a99e4edce453c8b106c9cb9c0d4246e0b22a82
ihsangunardi <ihsangunardi1711@gmail.com>
2025-11-25 12:42:13 +0800

    import random dan variabel

--- Tebak_angka.py
+++ Tebak_angka.py
@@ -4,30 +4,3 @@ paling_kecil = 1
 paling_besar = 50
 angka_rahasia = random.randint(paling_kecil, paling_besar)
 batas_percobaan = 7
-
-def cek_tebakan(tebak, angka_rahasia):
-    if tebak == angka_rahasia:
-        return "Selamat!! Anda Benar"
-    elif tebak < angka_rahasia:
-        return "Terlalu Kecil"
-    else:
-        return "Terlalu Besar"
-
-def mainkan_permainan():
-    percobaan = 0
-    menang = False
-
-    while percobaan < batas_percobaan:
-        percobaan += 1
-        tebakan = minta_tebakan()
-        hasil = cek_tebakan(tebakan, angka_rahasia)
-
-        if hasil == "Benar":
-            print(f"Selamat! Anda berhasil menebak angka rahasia {angka_rahasia} dalam {percobaan} percobaan.")
-            menang = True
-            break
-        else:
-            print(f"{hasil}. Coba lagi!")
-
-    if not menang:
-        print(f"Maaf, percobaan Anda habis! Angka rahasianya adalah {angka_rahasia}.")
