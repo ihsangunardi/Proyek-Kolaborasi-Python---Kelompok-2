41a0d9aeb1c46282844c733d59b9dc74ef791d04
pariskevin59-crypto <parispubgm@gmail.com>
2025-11-25 17:23:30 +0800

    menambahkan fungsi menerima tebakan

--- Tebak_angka.py
+++ Tebak_angka.py
@@ -1,33 +1,10 @@
-import random
-
-paling_kecil = 1
-paling_besar = 50
-angka_rahasia = random.randint(paling_kecil, paling_besar)
-batas_percobaan = 7
-
-def cek_tebakan(tebak, angka_rahasia):
-    if tebak == angka_rahasia:
-        return "Selamat!! Anda Benar"
-    elif tebak < angka_rahasia:
-        return "Terlalu Kecil"
-    else:
-        return "Terlalu Besar"
-
-def mainkan_permainan():
-    percobaan = 0
-    menang = False
-
-    while percobaan < batas_percobaan:
-        percobaan += 1
-        tebakan = minta_tebakan()
-        hasil = cek_tebakan(tebakan, angka_rahasia)
-
-        if hasil == "Benar":
-            print(f"Selamat! Anda berhasil menebak angka rahasia {angka_rahasia} dalam {percobaan} percobaan.")
-            menang = True
-            break
-        else:
-            print(f"{hasil}. Coba lagi!")
-
-    if not menang:
-        print(f"Maaf, percobaan Anda habis! Angka rahasianya adalah {angka_rahasia}.")
+def get_guess():
+    while True:
+        try:
+            tebak = int(f"tebak angka dari {paling_kecil} sampai {paling_besar}:")
+            if paling_kecil <= tebak <= paling_besar:
+                return tebak
+            else:
+                print("input tidak valid. harap masukkan angka dalam rentang yang ditentukan.")
+        except ValueError:
+            print("input tidak valid.harap masukkan angka yang valid.")
